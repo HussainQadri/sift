310067c136312ce18607f052524f76ae342d5b7b
Hussain Qadri <hussain.bqadri@gmail.com>
2026-05-18 21:04:07 +0100

    Fix inconsistent path
    Using main.rs for tree, but treesitter_parse.rs for function extraction.
    Mismatch.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ mod similarity;
 mod treesitter_parse;
 use std::fs;
 fn main() -> anyhow::Result<()> {
-    let tree = treesitter_parse::parser_demo("src/main.rs");
-    println!("{:?}", tree.root_node());
     let path = "src/treesitter_parse.rs";
+    let tree = treesitter_parse::parser_demo(path);
     let source_code = fs::read_to_string(path).expect("Failed to read source file");
     treesitter_parse::extract_functions(tree.root_node(), &source_code);
 
