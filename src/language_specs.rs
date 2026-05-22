071aac6157a26e04d1bc9a2521c56bb31435b374
Hussain Qadri <hussain.bqadri@gmail.com>
2026-05-22 04:25:57 +0100

    Fix queries for current languages

--- src/language_specs.rs
+++ src/language_specs.rs
@@ -3,25 +3,25 @@ use std::path::PathBuf;
 pub fn rust_spec() -> LanguageSpec {
     LanguageSpec {
         language: tree_sitter_rust::LANGUAGE.into(),
-        function_header_query: r#"
-              (function_item) @function
-          "#,
+        function_query: r#"(function_item body: (block) @body) @function"#,
     }
 }
 
 pub fn python_spec() -> LanguageSpec {
     LanguageSpec {
         language: tree_sitter_python::LANGUAGE.into(),
-        function_header_query: r#"
-              (function_definition) @function
+        function_query: r#"
+              (function_definition
+                body: (block) @body) @function
           "#,
     }
 }
 pub fn cpp_spec() -> LanguageSpec {
     LanguageSpec {
         language: tree_sitter_cpp::LANGUAGE.into(),
-        function_header_query: r#"
-              (function_definition) @function
+        function_query: r#"
+              (function_definition
+                body: (compound_statement) @body) @function
           "#,
     }
 }
@@ -32,12 +32,12 @@ pub fn spec_for_file(path: &PathBuf) -> anyhow::Result<LanguageSpec> {
         Some("rs") => Ok(rust_spec()),
         Some("py") => Ok(python_spec()),
         Some("cpp") => Ok(cpp_spec()),
-        Some(ext) => anyhow::bail!("Unsupported file extension"),
+        Some(_ext) => anyhow::bail!("Unsupported file extension"),
         None => anyhow::bail!("File has no extension"),
     }
 }
 
 pub struct LanguageSpec {
     pub(crate) language: tree_sitter::Language,
-    pub(crate) function_header_query: &'static str,
+    pub(crate) function_query: &'static str,
 }
