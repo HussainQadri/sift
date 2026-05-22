e98adf0c3e3eadae1c2100224b61c8fa07e2a818
Hussain Qadri <hussain.bqadri@gmail.com>
2026-05-22 05:44:10 +0100

    Return ordered search results based on query
    Working prototype, we can now output actual semantic matches.

--- src/embeddings_generator.rs
+++ src/embeddings_generator.rs
@@ -10,12 +10,24 @@ pub fn create_function_header_embeddings(
     let result = headers
         .into_iter()
         .zip(function_header_embeddings)
-        .map(|(header, embedding)| FunctionEmbedding { header, embedding })
+        .map(|(header, embedding)| FunctionEmbedding {
+            header,
+            header_embedding: embedding,
+        })
         .collect();
     Ok(result)
 }
 
 pub struct FunctionEmbedding {
-    header: String,
-    embedding: Vec<f32>,
+    pub(crate) header: String,
+    pub(crate) header_embedding: Vec<f32>,
+}
+
+pub fn create_embedding(keywords: &String) -> anyhow::Result<Vec<f32>> {
+    let mut model = TextEmbedding::try_new(Default::default())?;
+
+    let mut keyword_embedding = model.embed(vec![keywords], None)?;
+    let query_embedding = keyword_embedding.pop().unwrap();
+
+    Ok(query_embedding)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ mod similarity;
 mod treesitter_parse;
 use std::fs;
 
+use crate::similarity::cosine_similarity;
+
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Ingest { path: std::path::PathBuf },
+    Search {
+        path: std::path::PathBuf,
+        keywords: String,
+    },
 }
 
 #[derive(Parser)]
@@ -19,17 +24,28 @@ struct Cli {
 fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
     match args.commands {
-        Commands::Ingest { path } => {
+        // big mess here, allow embeddings to persist -> todo
+        Commands::Search { path, keywords } => {
             let spec = language_specs::spec_for_file(&path)?;
             let tree = treesitter_parse::parser_demo(&path, &spec);
             let source_code = fs::read_to_string(&path).expect("Failed to read source file");
             let headers =
                 treesitter_parse::extract_function_headers(tree.root_node(), &source_code, &spec);
+            let header_embeddings =
+                embeddings_generator::create_function_header_embeddings(headers)?;
+            let query_vector = embeddings_generator::create_embedding(&keywords)?;
 
-            for header in headers {
-                println!("{}", header);
+            let mut header_similarity_vector = Vec::new();
+            for embedding_struct in header_embeddings {
+                let cosine_result =
+                    cosine_similarity(&embedding_struct.header_embedding, &query_vector);
+                header_similarity_vector.push((embedding_struct.header, cosine_result));
             }
-            treesitter_parse::extract_functions(tree.root_node(), &source_code, &spec);
+
+            header_similarity_vector.sort_by(|a, b| b.1.total_cmp(&a.1));
+            for pair in header_similarity_vector {
+                println!("{}", pair.0);
+            }
         }
     }
 
