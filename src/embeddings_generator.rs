ce560d20617a5a349f879a15abda26c9b5ad2b69
Hussain Qadri <hussain.bqadri@gmail.com>
2026-05-28 01:13:34 +0100

    Store model in .cache folder instead of downloading model for each project

--- src/embeddings_generator.rs
+++ src/embeddings_generator.rs
@@ -1,16 +1,28 @@
+use std::path::PathBuf;
+
 use fastembed::{EmbeddingModel, InitOptions, TextEmbedding};
 
 pub fn create_function_embedding(texts: Vec<&String>) -> anyhow::Result<Vec<Vec<f32>>> {
-    let mut model =
-        TextEmbedding::try_new(InitOptions::new(EmbeddingModel::JinaEmbeddingsV2BaseCode))?;
+    let mut model = create_embedding_model()?;
     let embeddings = model.embed(texts, None)?;
     Ok(embeddings)
 }
 
 pub fn create_query_embedding(keywords: &String) -> anyhow::Result<Vec<f32>> {
-    let mut model =
-        TextEmbedding::try_new(InitOptions::new(EmbeddingModel::JinaEmbeddingsV2BaseCode))?;
+    let mut model = create_embedding_model()?;
     let mut keyword_embedding = model.embed(vec![keywords], None)?;
     let query_embedding = keyword_embedding.pop().unwrap();
     Ok(query_embedding)
+}
+fn model_cache_dir() -> PathBuf {
+    dirs::cache_dir()
+        .unwrap_or_else(|| PathBuf::from(".cache"))
+        .join("sift")
+        .join("fastembed")
+}
+fn create_embedding_model() -> anyhow::Result<TextEmbedding> {
+    let options = InitOptions::new(EmbeddingModel::JinaEmbeddingsV2BaseCode)
+        .with_cache_dir(model_cache_dir());
+
+    TextEmbedding::try_new(options)
 }
