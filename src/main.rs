d2a0bfb58d96d9c6b90dd8a0a6cf4ec4391e6208
Hussain Qadri <hussain.bqadri@gmail.com>
2026-05-31 22:09:30 +0100

    change to ignore::Walk for dir walking instead of custom recursive function
    This crate automatically ignores files in .gitignore

--- src/main.rs
+++ src/main.rs
@@ -47,30 +47,31 @@ struct Cli {
     commands: Option<Commands>,
 }
 
-fn recursive_ingest_dir(
+fn ingest_directory(
     model: &mut TextEmbedding,
     path: &std::path::PathBuf,
 ) -> anyhow::Result<Vec<index::IndexedFunction>> {
     let mut all_indexed_functions = Vec::new();
 
-    for resource_entry_result in fs::read_dir(path)? {
-        let entry = resource_entry_result?;
+    for result in Walk::new(path) {
+        let entry = match result {
+            Ok(entry) => entry,
+            Err(err) => {
+                eprintln!("Invalid directory entry: {err}");
+                continue;
+            }
+        };
         let file_path = entry.path();
 
-        if file_path.is_dir() {
-            let indexed_functions = recursive_ingest_dir(model, &file_path)?;
-            all_indexed_functions.extend(indexed_functions);
-        }
-
-        let spec = match language_specs::spec_for_file(&file_path) {
+        let spec = match language_specs::spec_for_file(file_path) {
             Ok(spec) => spec,
             Err(_) => continue,
         };
 
-        let tree = treesitter_parse::generate_tree(&file_path, &spec);
-        let source_code = fs::read_to_string(&file_path)?;
+        let tree = treesitter_parse::generate_tree(file_path, &spec);
+        let source_code = fs::read_to_string(file_path)?;
         let functions = treesitter_parse::extract_functions(tree.root_node(), &source_code, &spec);
-        let indexed_functions = index::create_indexed_functions(model, functions, &file_path)?;
+        let indexed_functions = index::create_indexed_functions(model, functions, file_path)?;
         all_indexed_functions.extend(indexed_functions);
     }
 
@@ -87,7 +88,7 @@ fn main() -> anyhow::Result<()> {
                 None => std::path::PathBuf::from("."),
             };
 
-            let all_indexed_functions = recursive_ingest_dir(&mut model, &target_path)?;
+            let all_indexed_functions = ingest_directory(&mut model, &target_path)?;
             index::save_index(&all_indexed_functions)?;
         }
         None => {
