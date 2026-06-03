294059f082d1313bc94969f1f4fbec601a2fc189
Hussain Qadri <hussain.bqadri@gmail.com>
2026-06-03 02:19:39 +0100

    search.rs contains query search

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::cli::Commands;
+use crate::{cli::Commands, search::query_search};
+mod search;
 use clap::Parser;
 mod cli;
 mod cli_output;
@@ -8,11 +9,9 @@ mod ingest;
 mod language_specs;
 mod similarity;
 mod treesitter_parse;
-use crate::similarity::cosine_similarity;
-use std::path::Path;
 
 fn main() -> anyhow::Result<()> {
-    let args = cli::Cli::parse();
+    let args: cli::Cli = cli::Cli::parse();
     match args.commands {
         Some(Commands::Ingest { path }) => {
             let mut model = embeddings_generator::create_embedding_model()?;
@@ -27,31 +26,7 @@ fn main() -> anyhow::Result<()> {
         }
 
         None => {
-            let keywords = args.keywords.expect("Please enter a search query");
-            let top_k_results = args.top;
-            let query = embeddings_generator::create_query_embedding(&keywords)?;
-            let loaded_indexed_functions = index::load_index()?;
-            let mut result: Vec<(index::IndexedFunction, f32)> = loaded_indexed_functions
-                .into_iter()
-                .map(|indexed_function| {
-                    let score = cosine_similarity(&query, &indexed_function.embedding);
-
-                    (indexed_function, score)
-                })
-                .collect();
-            result.sort_by(|a, b| b.1.total_cmp(&a.1));
-
-            for (indexed_function, score) in result.iter().take(top_k_results) {
-                println!("{:.3} {}: ", score, indexed_function.path);
-                let extension = Path::new(&indexed_function.path)
-                    .extension()
-                    .and_then(|ext| ext.to_str())
-                    .unwrap_or("");
-
-                print!("\x1b[32m{}:\x1b[0m ", indexed_function.line_number);
-                cli_output::print_highlighted(&indexed_function.header, extension);
-                println!("\n");
-            }
+            query_search(args)?;
         }
     }
 
--- src/search.rs
+++ src/search.rs
@@ -1,1 +1,36 @@
-todo!("add search logic here");
+use std::path::Path;
+
+use crate::cli;
+use crate::cli_output;
+use crate::embeddings_generator;
+use crate::index;
+use crate::similarity::cosine_similarity;
+
+pub fn query_search(args: cli::Cli) -> anyhow::Result<()> {
+    let keywords = args.keywords.expect("Please enter a search query");
+    let top_k_results = args.top;
+    let query = embeddings_generator::create_query_embedding(&keywords)?;
+    let loaded_indexed_functions = index::load_index()?;
+    let mut result: Vec<(index::IndexedFunction, f32)> = loaded_indexed_functions
+        .into_iter()
+        .map(|indexed_function| {
+            let score = cosine_similarity(&query, &indexed_function.embedding);
+
+            (indexed_function, score)
+        })
+        .collect();
+    result.sort_by(|a, b| b.1.total_cmp(&a.1));
+
+    for (indexed_function, score) in result.iter().take(top_k_results) {
+        println!("{:.3} {}: ", score, indexed_function.path);
+        let extension = Path::new(&indexed_function.path)
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .unwrap_or("");
+
+        print!("\x1b[32m{}:\x1b[0m ", indexed_function.line_number);
+        cli_output::print_highlighted(&indexed_function.header, extension);
+        println!("\n");
+    }
+    Ok(())
+}
