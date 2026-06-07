0137d7196f9303d3fd2e948ffd9ac8780ae6cfa2
Hussain Qadri <hussain.bqadri@gmail.com>
2026-06-07 02:53:52 +0100

    greedy search for closest neighbour to query vector
    not the final approach, this will evolve into search with
    ef_construction

--- src/hnsw.rs
+++ src/hnsw.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+use crate::similarity;
 pub struct Node {
     id: usize,
     embedding: Vec<f32>,
@@ -8,21 +9,50 @@ pub struct Node {
 pub struct HnswIndex {
     nodes: Vec<Node>,
     entry_point: Option<usize>,
+    m: usize,
 }
 
 pub fn insert(index: &mut HnswIndex, embedding_vec: Vec<f32>) {
     // empty index case
+    let mut node_to_insert = Node {
+        id: index.nodes.len(),
+        embedding: embedding_vec,
+        neighbours: Vec::new(),
+    };
     if index.nodes.is_empty() {
-        let node_to_insert = Node {
-            id: 0,
-            embedding: embedding_vec,
-            neighbours: Vec::new(),
-        };
-
         index.nodes.push(node_to_insert);
         index.entry_point = Some(0);
     } else {
-        todo!("Insert case when index is non-empty");
+        let mut current_id = index.entry_point.unwrap();
+
+        loop {
+            let current_similarity = similarity::cosine_similarity(
+                &index.nodes[current_id].embedding,
+                &node_to_insert.embedding,
+            );
+
+            let most_similar_neighbours = calculate_most_similiar_neighbours(
+                &index.nodes[current_id],
+                &node_to_insert,
+                index,
+            );
+
+            if most_similar_neighbours.is_empty() {
+                break;
+            }
+
+            let best_neighbour_id = most_similar_neighbours[0].0;
+            let best_neighbour_similarity = most_similar_neighbours[0].1;
+
+            if best_neighbour_similarity > current_similarity {
+                current_id = best_neighbour_id;
+            } else {
+                break;
+            }
+        }
+        node_to_insert.neighbours.push(current_id);
+        index.nodes[current_id].neighbours.push(node_to_insert.id);
+        index.nodes.push(node_to_insert);
     }
 }
 
