b09a867aed94dfd43f3866c8e6823b9c142d593d
Hussain Qadri <hussain.bqadri@gmail.com>
2026-06-07 01:55:23 +0100

    most similar neighbours calculated for index

--- src/hnsw.rs
+++ src/hnsw.rs
@@ -25,3 +25,26 @@ pub fn insert(index: &mut HnswIndex, embedding_vec: Vec<f32>) {
         todo!("Insert case when index is non-empty");
     }
 }
+
+pub fn calculate_most_similiar_neighbours(
+    current_node: &Node,
+    query_node: &Node,
+    all_nodes: &HnswIndex,
+) -> Vec<(usize, f32)> {
+    let node_neighbors = &current_node.neighbours;
+    let mut result: Vec<(usize, f32)> = node_neighbors
+        .iter()
+        .map(|&node_id| {
+            (
+                node_id,
+                similarity::cosine_similarity(
+                    &all_nodes.nodes[node_id].embedding,
+                    &query_node.embedding,
+                ),
+            )
+        })
+        .collect();
+
+    result.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    result
+}
