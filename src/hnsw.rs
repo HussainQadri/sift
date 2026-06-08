68d4866cc2354d6cbc607538693b98d85c1fadbe
Hussain Qadri <hussain.bqadri@gmail.com>
2026-06-08 01:34:46 +0100

    refactor search while inserting into search_greedy()

--- src/hnsw.rs
+++ src/hnsw.rs
@@ -23,37 +23,42 @@ pub fn insert(index: &mut HnswIndex, embedding_vec: Vec<f32>) {
         index.nodes.push(node_to_insert);
         index.entry_point = Some(0);
     } else {
-        let mut current_id = index.entry_point.unwrap();
-
-        loop {
-            let current_similarity = similarity::cosine_similarity(
-                &index.nodes[current_id].embedding,
-                &node_to_insert.embedding,
-            );
-
-            let most_similar_neighbours = calculate_most_similiar_neighbours(
-                &index.nodes[current_id],
-                &node_to_insert,
-                index,
-            );
-
-            if most_similar_neighbours.is_empty() {
-                break;
-            }
-
-            let best_neighbour_id = most_similar_neighbours[0].0;
-            let best_neighbour_similarity = most_similar_neighbours[0].1;
-
-            if best_neighbour_similarity > current_similarity {
-                current_id = best_neighbour_id;
-            } else {
-                break;
-            }
-        }
-        node_to_insert.neighbours.push(current_id);
-        index.nodes[current_id].neighbours.push(node_to_insert.id);
+        let closest_node_id = search_greedy(index, &node_to_insert);
+        node_to_insert.neighbours.push(closest_node_id);
+        index.nodes[closest_node_id]
+            .neighbours
+            .push(node_to_insert.id);
         index.nodes.push(node_to_insert);
     }
+}
+
+fn search_greedy(index: &HnswIndex, node_to_insert: &Node) -> usize {
+    let mut current_id = index.entry_point.unwrap();
+
+    loop {
+        let current_similarity = similarity::cosine_similarity(
+            &index.nodes[current_id].embedding,
+            &node_to_insert.embedding,
+        );
+
+        let most_similar_neighbours =
+            calculate_most_similiar_neighbours(&index.nodes[current_id], &node_to_insert, index);
+
+        if most_similar_neighbours.is_empty() {
+            break;
+        }
+
+        let best_neighbour_id = most_similar_neighbours[0].0;
+        let best_neighbour_similarity = most_similar_neighbours[0].1;
+
+        if best_neighbour_similarity > current_similarity {
+            current_id = best_neighbour_id;
+        } else {
+            break;
+        }
+    }
+
+    current_id
 }
 
 pub fn calculate_most_similiar_neighbours(
